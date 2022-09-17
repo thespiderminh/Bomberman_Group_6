e7647a60e9326bc5d1a36ae81ae7fc8fae423f9d
HuyBuiT <100178420+HuyBuiT@users.noreply.github.com>
2022-09-17 14:40:27 +0700

    main game

---
+++ src/main/java/uet/oop/bomberman/entities/Bomber.java
@@ -0,0 +1,15 @@
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+
+public class Bomber extends Entity {
+
+    public Bomber(int x, int y, Image img) {
+        super( x, y, img);
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
