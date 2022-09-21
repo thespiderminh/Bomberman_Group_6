e6e403c56a4f0cdcb77c073a724511a148e89190
ASUS <minhwhisky700@gmail.com>
2022-09-21 09:44:48 +0700

    Enemy class added

---
+++ src/main/java/uet/oop/bomberman/entities/Enemy.java
@@ -0,0 +1,14 @@
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+
+public class Enemy extends Entity {
+    public Enemy(int x, int y, Image img) {
+        super( x, y, img);
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
