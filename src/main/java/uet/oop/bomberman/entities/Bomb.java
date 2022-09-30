2260443fef52a03247f368b008c2b9093e7c3596
thespiderminh <100653220+thespiderminh@users.noreply.github.com>
2022-09-30 15:49:40 +0700

    Bomb nổ nma chưa lan ra + vẫn lỗi tràn dữ liệu (chịu)

--- src/main/java/uet/oop/bomberman/entities/Bomb.java
+++ src/main/java/uet/oop/bomberman/entities/Bomb.java
@@ -7,17 +7,20 @@ import uet.oop.bomberman.graphics.Sprite;
 import java.util.Arrays;
 import java.util.List;
 
+import static uet.oop.bomberman.BombermanGame.getEntities;
+
 public class Bomb extends Entity {
     protected static int numberOfBombs = 3;
     protected static int numberOfBombsOnScreen = 0;
     private static final long	BOMB_TIME	= 2000000000L;
     private long startTime	= 0;
     private long existTime = 0;
+    private long explodeTime = 0;
     private boolean	onScreen	= false;
+    private boolean exploding = false;
     private int typeOfBomb = 0;
-    private List<Image> bombAnimation = Arrays.asList(Sprite.bomb.getFxImage(),Sprite.bomb_1.getFxImage(), Sprite.bomb_2.getFxImage());
-
-
+    private final List<Image> bombAnimation = Arrays.asList(Sprite.bomb.getFxImage(),Sprite.bomb_1.getFxImage(), Sprite.bomb_2.getFxImage());
+    private final List<Image> bombExplosionAnimation = Arrays.asList(Sprite.bomb_exploded.getFxImage(), Sprite.bomb_exploded1.getFxImage(), Sprite.bomb_exploded2.getFxImage());
     public Bomb(int x, int y, Image img) {
         super(x, y, img);
     }
@@ -30,21 +33,34 @@ public class Bomb extends Entity {
             existTime = now;
         }
 
-        if (onScreen) {
+        if (onScreen && exploding == false) {
             if( now - startTime >= 300000000L) {
-                this.img = bombAnimation.get(typeOfBomb);
+                img = bombAnimation.get(typeOfBomb);
                 typeOfBomb = (typeOfBomb + 1) % 3;
                 startTime = now;
             }
             
             if (now - existTime > 4000000000L) {
-                explode();
+                exploding = true;
+                explodeTime = now;
+                typeOfBomb = 0;
             }
+        }
+        if (exploding == true) {
+            explode(now);
         }
     }
 
-    private void explode() {
-        this.img = null;
+    private void explode(long now) {
+        if( now - explodeTime >= 150000000L) {
+            if(typeOfBomb < 2) {
+                typeOfBomb++;
+                img = bombExplosionAnimation.get(typeOfBomb);
+                explodeTime = now;
+            } else {
+                getEntities().remove(this);
+            }
+        }
         numberOfBombsOnScreen--;
     }
 
