792322bd33a3731f23c300766ab12b463fc9b33c
HuyBuiT <huyqweasd433@gmail.com>
2022-11-02 16:29:45 +0700

    random map

--- src/main/java/uet/oop/bomberman/graphics/SpriteSheet.java
+++ src/main/java/uet/oop/bomberman/graphics/SpriteSheet.java
@@ -4,7 +4,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
-
+import java.util.Random;
 /**
  * Tất cả sprite (hình ảnh game) được lưu trữ vào một ảnh duy nhất
  * Class này giúp lấy ra các sprite riêng từ 1 ảnh chung duy nhất đó
@@ -15,9 +15,10 @@ public class SpriteSheet {
 	public final int SIZE;
 	public int[] _pixels;
 	public BufferedImage image;
-
-	public static SpriteSheet tiles = new SpriteSheet("/textures/classic4.png", 256);// clasic 2 3 4 choose what you want XD
-	
+	private static Random random = new Random();
+	static int no = random.nextInt(2) +2 ;
+	public static String level = "/textures/classic"+no+".png";
+	public static SpriteSheet tiles = new SpriteSheet(level, 256);// clasic 2 3 4 choose what you want XD
 	public SpriteSheet(String path, int size) {
 		_path = path;
 		SIZE = size;
