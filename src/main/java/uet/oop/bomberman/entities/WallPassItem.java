9a3b3ab35b11c795476b73734e8c9447ce1e4802
thespiderminh <100653220+thespiderminh@users.noreply.github.com>
2022-11-02 15:36:42 +0700

    abc

---
+++ src/main/java/uet/oop/bomberman/entities/WallPassItem.java
@@ -0,0 +1,15 @@
+package uet.oop.bomberman.entities;
+
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+public class WallPassItem extends Item {
+	public WallPassItem(int x, int y, Image img) {
+		super( x, y, img);
+	}
+
+	@Override
+	public void update(Scene scene, long now) {
+
+	}
+}
